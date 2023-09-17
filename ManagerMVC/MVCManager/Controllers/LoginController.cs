13ea24c378f748bd4394de1d65b20b6e9ee67526
fachoquecota <62406549+fachoquecota@users.noreply.github.com>
2023-09-17 04:31:59 -0500

    Sección añadida

---
+++ ManagerMVC/MVCManager/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCManager.Models;
+
+namespace MVCManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Esto evita que se utilice el _Layout
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Aquí puedes implementar la lógica de autenticación
+            // Por ejemplo, verifica si el usuario y la contraseña son correctos
+
+            // Si la autenticación es exitosa, redirige a la página de inicio
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
---
+++ ManagerMVC/MVCManager/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCManager.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El campo Usuario es requerido")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "El campo Contraseña es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
