008023f149ccf8f0487557a94622f07776b56a46
fachoquecota <62406549+fachoquecota@users.noreply.github.com>
2023-09-19 00:03:32 -0500

    opciones

---
+++ ManagerMVC/MVCManager/Controllers/SizeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCManager.Controllers
+{
+    public class SizeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
