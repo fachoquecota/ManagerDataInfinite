c17f2f7c2502a68555307a830d216a15d393fb29
fachoquecota <62406549+fachoquecota@users.noreply.github.com>
2023-10-30 03:28:33 -0500

    .

--- ManagerMVC/MVCManager/Controllers/ModelosController.cs
+++ ManagerMVC/MVCManager/Controllers/ModelosController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Text;
+
 namespace MVCManager.Controllers
 {
     public class ModelosController : Controller
@@ -19,14 +21,99 @@ namespace MVCManager.Controllers
             }
             return View(modelos);
         }
+        public async Task<ModeloProducto> GetModeloProductoByID(int id)
+        {
+            ModeloProducto modelo = new ModeloProducto();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"http://apiprosalesmanager.somee.com/api/Productos/GETModeloProductosByIDCrud?idProducto={id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var responseObject = JsonConvert.DeserializeObject<ApiResponse>(apiResponse);
+                    modelo = responseObject.result.FirstOrDefault();
+                }
+            }
+            return modelo;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateModeloProducto([FromBody] ModeloProductoUpdate modeloProducto)
+
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var model = new
+                {
+                    idModeloProducto = modeloProducto.idModeloProducto,
+                    descripcion = modeloProducto.descripcion
+                };
+                var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PutAsync("http://apiprosalesmanager.somee.com/api/Productos/PUTUpdateModeloProducto", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> InsertModeloProducto([FromBody] ModeloProductoInsert modeloProducto)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(modeloProducto), Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/POSTInsertModeloProducto", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteModeloProducto(int idModeloProducto)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.DeleteAsync($"http://apiprosalesmanager.somee.com/api/Productos/DELETEDeleteModeloProducto?idModeloProducto={idModeloProducto}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+        }
     }
 }
+public class ModeloProductoUpdate
+{
+    public int idModeloProducto { get; set; }
+    public string descripcion { get; set; }
+}
+
 public class ApiResponse
 {
     public List<ModeloProducto> result { get; set; }
 }
 
 public class ModeloProducto
+{
+    public int idModeloProducto { get; set; }
+    public string descripcion { get; set; }
+}
+public class ModeloProductoInsert
 {
     public int idModeloProducto { get; set; }
     public string descripcion { get; set; }
--- ManagerMVC/MVCManager/Controllers/ProductosController.cs
+++ ManagerMVC/MVCManager/Controllers/ProductosController.cs
@@ -594,154 +594,7 @@ namespace MVCManager.Controllers
         public async Task<IActionResult> CreateProducto(ProductoModel model, IFormFile nuevaImagen, string SizeUpdateorDelete, string TagUpdateInfo, string ColorUpdateInfoJson)
         {
             model.IdProducto = 0;
-            var colorUpdateInfo = JsonConvert.DeserializeObject<ColorUpdateInfo>(ColorUpdateInfoJson);
-
-            var colorsToAdd = colorUpdateInfo.colorsToAdd;
-            var colorsToDelete = colorUpdateInfo.colorsToDelete;
-
-            //COLOR DETALLE
-            using (var httpClient = new HttpClient())
-            {
-
-                // Añadir nuevos colores
-                foreach (var color in colorsToAdd)
-                {
-                    var content = new StringContent(JsonConvert.SerializeObject(new
-                    {
-                        idColorDetalle = 0,
-                        idProducto = model.IdProducto,
-                        idColor = color.idColor,
-                        codigo = string.Empty,
-                        activo = color.isActive
-                    }), Encoding.UTF8, "application/json");
-
-                    var response = await httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/PostCrudColorDetalle", content);
-                    // Puedes manejar la respuesta si lo necesitas.
-                }
-
-            }
-
-            //TAG
-            // Deserializar la información de los tags.
-            var tagUpdateInfo = JsonConvert.DeserializeObject<TagUpdateInfo>(TagUpdateInfo);
-
-            // Luego, puedes acceder a las listas de tags actualizados, nuevos y eliminados:
-            var tagsToCreate = tagUpdateInfo.tagsToAdd;
-
-
-            // Crear nuevos tags
-            using (var httpClient = new HttpClient())
-            {
-                foreach (var tag in tagsToCreate)
-                {
-                    var content = new StringContent(JsonConvert.SerializeObject(new
-                    {
-                        idTags = 0,
-                        idProducto = model.IdProducto,
-                        descripcion = tag.description,
-                        activo = tag.isActive
-                    }), Encoding.UTF8, "application/json");
-
-                    var response = await httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/PostCrudTag", content);
-                    // Puedes manejar la respuesta si lo necesitas.
-                }
-            }
-            // SIZE
-            // 1. Deserializar SizeUpdateorDelete
-            var sizeData = JsonConvert.DeserializeObject<dynamic>(SizeUpdateorDelete);
-            var toCreate = sizeData.toCreate.ToObject<List<dynamic>>();
-
-            // 4. Procesar nuevos registros
-            foreach (var record in toCreate)
-            {
-                var idSize = record.idSize;
-                var activo = record.activo;
-
-                using (var httpClient = new HttpClient())
-                {
-                    var newData = new
-                    {
-                        idSizeDetalle = 0,
-                        idSize = idSize,
-                        idProducto = model.IdProducto, // Supongo que el IdProducto proviene del modelo, ajusta si es necesario.
-                        descripcion = "test api insert",
-                        activo = activo
-                    };
-
-                    var content = new StringContent(JsonConvert.SerializeObject(newData), Encoding.UTF8, "application/json");
-                    var response = await httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/PostCrudSizeDetalle", content);
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        Console.WriteLine($"Nuevo SizeDetalle con idSize = {idSize} agregado exitosamente.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Error al agregar SizeDetalle con idSize = {idSize}. Respuesta: {response.StatusCode} - {response.ReasonPhrase}");
-                    }
-                }
-
-            }
-
-
-            // IMAGENES
-            // 2. Manejar y mostrar la ruta de las imágenes nuevas
-            if (_tempImages.Count > 0)
-            {
-                var folderPath = Path.Combine("wwwroot", "Images", "ProductoDetalle", model.IdProducto.ToString());
-                if (!Directory.Exists(folderPath))
-                {
-                    Directory.CreateDirectory(folderPath);
-                }
-
-                Console.WriteLine("Imágenes nuevas:");
-                foreach (var imgData in _tempImages)
-                {
-                    var fileExtension = imgData.Type switch
-                    {
-                        "image/jpeg" => ".jpg",
-                        "image/png" => ".png",
-                        _ => ".jpg", // Valor predeterminado
-                    };
-
-                    var fileNameImagenDetalle = $"{Guid.NewGuid()}{fileExtension}";  // Genera un nombre único basado en Guid
-                    var fullPath = Path.Combine(folderPath, fileNameImagenDetalle);
-
-                    await System.IO.File.WriteAllBytesAsync(fullPath, imgData.ImageBytes);
-
-                    Console.WriteLine($"Imagen almacenada en: {fullPath}");
-
-                    // Enviar la imagen a la API
-                    var imageToPost = new
-                    {
-                        idImagenes = 0,
-                        idProducto = model.IdProducto,
-                        rutaImagen = $"images\\ProductoDetalle\\{model.IdProducto}\\",
-                        nombreImagen = fileNameImagenDetalle,
-                        activo = true
-                    };
-
-                    var json = JsonConvert.SerializeObject(imageToPost);
-                    var content = new StringContent(json, Encoding.UTF8, "application/json");
-                    var response = await _httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/PostCrudImagen", content);
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        Console.WriteLine("Imagen enviada a la API con éxito.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Hubo un error al enviar la imagen a la API.");
-                    }
-                }
-
-                // Limpiar las imágenes en memoria una vez enviadas
-                _tempImages.Clear();
-            }
-            else
-            {
-                Console.WriteLine("No se añadieron imágenes nuevas.");
-            }
+            
 
             var fileName = ContentDispositionHeaderValue.Parse(nuevaImagen.ContentDisposition).FileName.Trim('"');
             if (nuevaImagen == null)
@@ -773,7 +626,7 @@ namespace MVCManager.Controllers
                 //var responseBody = await responseA.Content.ReadAsStringAsync();
                 var responseBody = await responseA.Content.ReadAsStringAsync();
                 Console.WriteLine(responseBody);
-                int newIdProducto = int.Parse(await responseA.Content.ReadAsStringAsync());
+                int newIdProducto = int.Parse(await responseA.Content.ReadAsStringAsync()); // ID DE PRODUCTO 
                 var folderPath = Path.Combine("wwwroot", "images", "ProductoPrincipal", newIdProducto.ToString());
                 folderPath = folderPath.Replace(Path.DirectorySeparatorChar, '\\') + "\\";
                 if (!Directory.Exists(folderPath))
@@ -788,6 +641,156 @@ namespace MVCManager.Controllers
                     await nuevaImagen.CopyToAsync(stream);
                 }
                 Console.WriteLine("Respuesta del API: " + newIdProducto);
+
+                var colorUpdateInfo = JsonConvert.DeserializeObject<ColorUpdateInfo>(ColorUpdateInfoJson);
+
+                var colorsToAdd = colorUpdateInfo.colorsToAdd;
+                var colorsToDelete = colorUpdateInfo.colorsToDelete;
+
+                //COLOR DETALLE
+                using (var httpClient = new HttpClient())
+                {
+
+                    // Añadir nuevos colores
+                    foreach (var color in colorsToAdd)
+                    {
+                        var content = new StringContent(JsonConvert.SerializeObject(new
+                        {
+                            idColorDetalle = 0,
+                            idProducto = newIdProducto,
+                            idColor = color.idColor,
+                            codigo = string.Empty,
+                            activo = color.isActive
+                        }), Encoding.UTF8, "application/json");
+
+                        var response = await httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/PostCrudColorDetalle", content);
+                        // Puedes manejar la respuesta si lo necesitas.
+                    }
+
+                }
+
+                //TAG
+                // Deserializar la información de los tags.
+                var tagUpdateInfo = JsonConvert.DeserializeObject<TagUpdateInfo>(TagUpdateInfo);
+
+                // Luego, puedes acceder a las listas de tags actualizados, nuevos y eliminados:
+                var tagsToCreate = tagUpdateInfo.tagsToAdd;
+
+
+                // Crear nuevos tags
+                using (var httpClient = new HttpClient())
+                {
+                    foreach (var tag in tagsToCreate)
+                    {
+                        var content = new StringContent(JsonConvert.SerializeObject(new
+                        {
+                            idTags = 0,
+                            idProducto = newIdProducto,
+                            descripcion = tag.description,
+                            activo = tag.isActive
+                        }), Encoding.UTF8, "application/json");
+
+                        var response = await httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/PostCrudTag", content);
+                        // Puedes manejar la respuesta si lo necesitas.
+                    }
+                }
+                // SIZE
+                // 1. Deserializar SizeUpdateorDelete
+                var sizeData = JsonConvert.DeserializeObject<dynamic>(SizeUpdateorDelete);
+                var toCreate = sizeData.toCreate.ToObject<List<dynamic>>();
+
+                // 4. Procesar nuevos registros
+                foreach (var record in toCreate)
+                {
+                    var idSize = record.idSize;
+                    var activo = record.activo;
+
+                    using (var httpClient = new HttpClient())
+                    {
+                        var newData = new
+                        {
+                            idSizeDetalle = 0,
+                            idSize = idSize,
+                            idProducto = newIdProducto, 
+                            descripcion = "test api insert",
+                            activo = activo
+                        };
+
+                        var content = new StringContent(JsonConvert.SerializeObject(newData), Encoding.UTF8, "application/json");
+                        var response = await httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/PostCrudSizeDetalle", content);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine($"Nuevo SizeDetalle con idSize = {idSize} agregado exitosamente.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error al agregar SizeDetalle con idSize = {idSize}. Respuesta: {response.StatusCode} - {response.ReasonPhrase}");
+                        }
+                    }
+
+                }
+
+
+                // IMAGENES
+                // 2. Manejar y mostrar la ruta de las imágenes nuevas
+                if (_tempImages.Count > 0)
+                {
+                    var folderPathImagenes = Path.Combine("wwwroot", "Images", "ProductoDetalle", newIdProducto.ToString());
+                    if (!Directory.Exists(folderPathImagenes))
+                    {
+                        Directory.CreateDirectory(folderPathImagenes);
+                    }
+
+                    Console.WriteLine("Imágenes nuevas:");
+                    foreach (var imgData in _tempImages)
+                    {
+                        var fileExtension = imgData.Type switch
+                        {
+                            "image/jpeg" => ".jpg",
+                            "image/png" => ".png",
+                            _ => ".jpg", // Valor predeterminado
+                        };
+
+                        var fileNameImagenDetalle = $"{Guid.NewGuid()}{fileExtension}";  
+                        var fullPathImagenes = Path.Combine(folderPathImagenes, fileNameImagenDetalle);
+
+                        await System.IO.File.WriteAllBytesAsync(fullPathImagenes, imgData.ImageBytes);
+
+                        Console.WriteLine($"Imagen almacenada en: {fullPathImagenes}");
+
+                        // Enviar la imagen a la API
+                        var imageToPost = new
+                        {
+                            idImagenes = 0,
+                            idProducto = newIdProducto,
+                            rutaImagen = $"images\\ProductoDetalle\\{newIdProducto}\\",
+                            nombreImagen = fileNameImagenDetalle,
+                            activo = true
+                        };
+
+                        var json = JsonConvert.SerializeObject(imageToPost);
+                        var content = new StringContent(json, Encoding.UTF8, "application/json");
+                        var response = await _httpClient.PostAsync("http://apiprosalesmanager.somee.com/api/Productos/PostCrudImagen", content);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine("Imagen enviada a la API con éxito.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Hubo un error al enviar la imagen a la API.");
+                        }
+                    }
+
+                    // Limpiar las imágenes en memoria una vez enviadas
+                    _tempImages.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("No se añadieron imágenes nuevas.");
+                }
+
             }
             else
             {
