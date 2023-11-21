9daaf8927876cdb7b8a76a931ed8e3df050fce41
fachoquecota <62406549+fachoquecota@users.noreply.github.com>
2023-11-21 01:24:13 -0500

    .

---
+++ ManagerMVC/MVCManager/Controllers/ReporteVentasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCManager.Controllers
+{
+    public class ReporteVentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
